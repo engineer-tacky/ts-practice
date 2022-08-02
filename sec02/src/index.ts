7cdfa49650ad42457bc48e3188559d52841c4c3e
engineer_tacky <tsubasa801@gmail.com>
2022-08-02 23:12:56 +0900

    型の変換2

--- sec02/src/index.ts
+++ sec02/src/index.ts
@@ -6,8 +6,14 @@
 // x = '123';
 // console.log(x);
 
-// 型の変換方法
+// 型の変換方法1
+// let x = 123;
+// let y = '456';
+// x = +y;
+// console.log(x);
+
+// 型の変換方法2
 let x = 123;
 let y = '456';
-x = +y;
+x = Number(y);
 console.log(x);
